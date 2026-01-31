a98a6069fb069ba274b7fca34bb71fb6aac6847b
Balazs Petrikovics <bpetrikovics@gmail.com>
2026-01-31 23:39:50 +0100

    Fix DB cleanup thread issue at shutdown

--- app/database.py
+++ app/database.py
@@ -152,10 +152,8 @@ class DbCleanupManager:
     def stop(self):
         self.logger.info("Stopping DB retention thread")
         self._running = False
-        # FIXME: This is not optimal, should be handled by gunicorn worker hook
-        # In its current form it may result in errors at shutdown time
-        if self._thread is not None and self._thread.is_alive():
-            self._thread.join(timeout=0)
+        # Thread is daemon=True, so it will be cleaned up automatically on exit
+        # No need to join() which causes eventlet conflicts during shutdown
 
     def status(self) -> Dict[str, Any]:
         return {
